5d02106aa4d7113f9af83b93ee19068ca0df4cc9
anttioahopelto <anttiahop@gmail.com>
2025-03-05 19:51:40 +0200

    Use environment variables for database connection values

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,8 +5,8 @@ object Main extends App {
   val server = WebServiceBuilder.buildWebService(8080, classOf[WebService])
   server.start()
 
-  val jdbcUrl = ""
-  val pgUsername = ""
-  val pgPassword = ""
+  val jdbcUrl = sys.env.getOrElse("DB_URL", "")
+  val pgUsername = sys.env.getOrElse("DB_USERNAME", "")
+  val pgPassword = sys.env.getOrElse("DB_PASSWORD", "")
   DBSetup.setup(jdbcUrl, pgUsername, pgPassword)
 }
